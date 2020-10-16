0201d7e42da2fe2146275f8361daea4c2b54b167
Liudmila Kornilova <kornilova203@gmail.com>
2020-10-16 17:55:27 +0300

    v0.4

--- driver/src/main/java/com/intellij/CouchbaseJdbcDriver.java
+++ driver/src/main/java/com/intellij/CouchbaseJdbcDriver.java
@@ -64,7 +64,7 @@ public class CouchbaseJdbcDriver implements Driver {
     }
 
     String getVersion() {
-        return "0.3.0";
+        return "0.4.0";
     }
 
     @Override
@@ -74,7 +74,7 @@ public class CouchbaseJdbcDriver implements Driver {
 
     @Override
     public int getMinorVersion() {
-        return 3;
+        return 4;
     }
 
     @Override
